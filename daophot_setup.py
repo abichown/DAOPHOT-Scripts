c9388e86c01a9ed143a741bf4a06326771f27ea6
Abi Chown <ac833@pypc-astro-03.bath.ac.uk>
2018-03-12 15:23:05 +0000

    Initial commit

---
+++ daophot_setup.py
@@ -0,0 +1,6 @@
+'''
+Purpose: Really a practice of how to control DAOPHOT with Python but also looking to set up
+optional parameters ready to do photometry
+Written by: Abi Chown A.H.Chown@bath.ac.uk
+'''
+
