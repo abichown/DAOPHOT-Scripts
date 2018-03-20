f8029cfa89e87019d36e9966a5b2da83492cfd71
Abi Chown <A.H.Chown@bath.ac.uk>
2018-03-20 08:49:43 +0000

    Initial commit

---
+++ single_image_phot.py
@@ -0,0 +1,12 @@
+'''
+Purpose: Carry out photometry on a single image
+Written by: Abi Chown A.H.Chown@bath.ac.uk
+'''
+
+import sys
+import pexpect
+import shutil
+import os
+import fnmatch
+import re
+
