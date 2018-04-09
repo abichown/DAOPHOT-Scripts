d1ef185ea893e35052ec5e4495faf9764510a52c
Abi Chown <A.H.Chown@bath.ac.uk>
2018-04-09 13:30:13 +0100

    initial commit with outline of program

---
+++ psf_creation.py
@@ -0,0 +1,26 @@
+'''
+Purpose: Select candidate PSF stars from star list. Then let user manually create PSF model for 
+one of the dithers. Then copy this PSF model file to the remaining 9 dithers.
+Written by: Abi Chown A.H.Chown@bath.ac.uk
+'''
+
+# import modules
+
+
+# find files 
+
+
+# open daophot
+
+
+# select candidate stars
+
+
+# exit daophot
+
+
+# create psf model manually
+
+
+# copy psf model to other 9 dithers
+
