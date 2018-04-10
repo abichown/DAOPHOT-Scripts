0cf7dfeb705a7789ec2143c7b25c5e48774a266f
Abi Chown <A.H.Chown@bath.ac.uk>
2018-04-10 11:24:12 +0100

    Initial commit

---
+++ median_image.py
@@ -0,0 +1,21 @@
+'''
+Purpose: Create a medianed image from a set of BCDs. Will make a total of 5 medianed images.
+1. Ch1 d1-5 
+2. Ch2 d6-10 
+3. Ch1 d6-10
+4. Ch2 d1-5
+5. Ch1 d6-10 and Ch2 d1-5 combined
+Images 3-5 have target V* star present. Images 1 and 2 do not.
+Input is the same txt file 
+Written by: Abi Chown A.H.Chown@bath.ac.uk
+'''
+
+# Import modules
+
+# Make medianed image function to call
+
+# Set up data frame from txt file of stars to do it on
+
+# Loop over each row in txt file 
+
+# Make the 5 images described above 
