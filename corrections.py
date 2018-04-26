73b1afa489fc5969a35505a98a53e204f9e08954
Abi Chown <A.H.Chown@bath.ac.uk>
2018-04-26 13:49:45 +0100

    Initial commit

---
+++ corrections.py
@@ -0,0 +1,6 @@
+'''
+Purpose: Correct the instrumental magnitudes via zero point, standard aperture, aperture and location corrections
+Written by: Abi Chown A.H.Chown@bath.ac.uk
+'''
+
+import numpy as np 
