9b7d9c7b18903ba77134a0786c57e2dff1180c52
Abi Chown <A.H.Chown@bath.ac.uk>
2018-04-27 12:39:46 +0100

    Setup of files and df's

--- ave_mag.py
+++ ave_mag.py
@@ -7,6 +7,7 @@ Written by: Abi Chown A.H.Chown@bath.ac.uk
 
 # Import modules
 import sys
+import os
 import pandas as pd
 from math import sqrt, log10
 
@@ -18,3 +19,50 @@ df = pd.read_csv(sys.argv[1], header=None, delim_whitespace=True, names=['Galaxy
 for i in range(0, len(df)):
 	for j in [1,6]:
 
+		# INITIAL SETUP 
+
+		galaxy = df['Galaxy'][i]
+		target_name = df['Star'][i]
+
+		if df['Channel'][i] == 1:
+			channel = 1
+			wavelength = '3p6um'
+		elif df['Channel'][i] == 2:
+			channel = 2
+			wavelength = '4p5um'
+		else: wavelength = 'channel not defined'
+
+		if df['Epoch'][i] < 10:
+			epoch_number = '0' + str(df['Epoch'][i])
+		else: epoch_number = str(df['Epoch'][i])
+
+		if j == 1:
+			field = '1'
+		elif j == 6:
+			field = '2'
+		else: field = 'Invalid start dither'
+
+    	# Find absolute path of where images are
+		home = '/home/ac833/Data/'
+
+		cwd = home + str(galaxy) + '/BCD/' + target_name + '/ch' + str(df['Channel'][i]) + '/e' + str(epoch_number) + '/'
+		stem = target_name + '_' + wavelength + '_e' + epoch_number
+
+		# Change directory to where image is
+		os.chdir(cwd)
+
+		# Remove any previous runs of this particular script
+		if (os.path.isfile(stem+'_f'+field+'.ave')):
+			os.remove(stem+'_f'+field+'.ave')
+
+		# Import data
+		filename = stem + '_f' + field + '.alf'
+		df2 = pd.read_csv(filename, delim_whitespace=True, skiprows=3, header=None, names=['ID', 'X', 'Y', 'm_1', 'e_1', 'm_2', 'e_2', 'm_3', 'e_3', 'm_4', 'e_4', 'm_5', 'e_5', 'sharp', 'round'])	
+
+		# Get right f0 for the channel
+		if wavelength == '3p6um':
+			f0 = 280.9
+		if wavelength == '4p5um':
+			f0 = 179.7
+
+    	print "Here"	
