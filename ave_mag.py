b0291c8e7282964894a79bbff9609bb002454adc
Abi Chown <A.H.Chown@bath.ac.uk>
2018-04-27 12:25:18 +0100

    Read in star list

--- ave_mag.py
+++ ave_mag.py
@@ -6,6 +6,15 @@ Written by: Abi Chown A.H.Chown@bath.ac.uk
 '''
 
 # Import modules
+import sys
 import pandas as pd
 from math import sqrt, log10
+
+# Set up data frame from txt file of stars (sys.argv[1]) to do it on
+df = pd.read_csv(sys.argv[1], header=None, delim_whitespace=True, names=['Galaxy', 'Star','Channel','Epoch'])
+
+# Loop over each row in txt file
+# Also loop over the two dither combinations
+for i in range(0, len(df)):
+	for j in [1,6]:
 
