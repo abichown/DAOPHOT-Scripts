90d250b59d7ac5cc033d5318d667dd28781d8a0a
Abi Chown <A.H.Chown@bath.ac.uk>
2018-04-30 11:47:12 +0100

    Getting star info

--- get_mag.py
+++ get_mag.py
@@ -10,3 +10,37 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+
+# Read star list
+df = pd.read_csv('/home/ac833/DAOPHOT-Scripts/star_list.txt', header=None, delim_whitespace=True, names=['Galaxy', 'Star', 'Channel', 'Epoch'])
+
+# Create list of unique star entries in df
+stars = pd.unique(df['Star'])
+
+for star in stars:
+
+	# Find rows that match this star
+	matches = df.index[(df['Star']==star)==True].tolist()
+
+	# Just need the first occurrence to find Galaxy
+	index = matches[0]
+
+	# Then get galxy name
+	galaxy = df['Galaxy'][index]
+
+	# Wavelengths list
+	wavelengths = ['3p6um', '4p5um']
+
+	for wavelength in wavelengths:
+
+		# Get channel number
+		if wavelength == '3p6um':
+			channel = '1'
+			field = '2' # we want the field with the V* in
+		if wavelength == '4p5um':
+			channel = '2'
+			field = '1' # we want the field with the V* in
+
+		
+
+	print galaxy
