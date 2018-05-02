d687fe6d6d59c39a14df92524b21316430f65eed
Abi Chown <A.H.Chown@bath.ac.uk>
2018-05-02 13:22:46 +0100

    Main part done

--- corrections.py
+++ corrections.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd 
 from astropy.io import fits
 from math import log10 
+import os
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 				MAIN PART
@@ -19,14 +20,47 @@ for i in range(0, len(df)):
 	for j in [1,6]: 
 
 		# Initial setup
+		galaxy = df['Galaxy'][i]
+		target_name = df['Star'][i]
+
+		if df['Channel'] == 1:
+			channel = 1
+			wavelength = '3p6um'
+		elif df['Channel'] == 2:
+			channel = 2
+			wavelength = '4p5um'
+		else: channel = 'Invalid channel'
+
+		if df['Epoch'][i] < 10:
+			epoch_number = '0' + str(df['Epoch'][i])
+		else: epoch_number = str(df['Epoch'][i])
+
+		if j == 1:
+			field = 1
+		elif j == 6:
+			field = 2
+		else field = "Invalid field"
+
+		# Find absolute path of where images are
+		home = '/home/ac833/Data/'
+		cwd = home + str(galaxy) + '/BCD/' + target_name + '/ch' + str(df['Channel'][i]) + '/e' + str(epoch_number) + '/'
+
+		stem = target_name + '_' + wavelength + '_e' + epoch_number
+
+		# Change working directory to where the data is
+		os.chdir(cwd)
 
 		# Zero point
+		zero_point(i,j)
 
 		# Standard aperture
+		std_aper(i,j)
 
 		# Aperture correction
+		ap_corr(i,j)
 
 		# Location correction
+		loc_corr(i,j)
 
 # Zero point correction - applied to both ap and alf mags
 def zero_point():
