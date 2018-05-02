72b73723b704ce8c7bdd40b18f63129529c393ae
Abi Chown <A.H.Chown@bath.ac.uk>
2018-05-02 13:39:20 +0100

    More setup

--- corrections.py
+++ corrections.py
@@ -8,13 +8,14 @@ import pandas as pd
 from astropy.io import fits
 from math import log10 
 import os
+import sys
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 				MAIN PART
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 # Read in list of stars that need correcting
-df = pd.read_csv('/home/ac833/DAOPHOT-Scripts/star_list.txt', header=None, delim_whitespace=True, names=['Galaxy', 'Star', 'Channel', 'Epoch'])
+df = pd.read_csv(sys.argv[1], header=None, delim_whitespace=True, names=['Galaxy', 'Star', 'Channel', 'Epoch'])
 
 for i in range(0, len(df)):
 	for j in [1,6]: 
@@ -23,10 +24,10 @@ for i in range(0, len(df)):
 		galaxy = df['Galaxy'][i]
 		target_name = df['Star'][i]
 
-		if df['Channel'] == 1:
+		if df['Channel'][i] == 1:
 			channel = 1
 			wavelength = '3p6um'
-		elif df['Channel'] == 2:
+		elif df['Channel'][i] == 2:
 			channel = 2
 			wavelength = '4p5um'
 		else: channel = 'Invalid channel'
@@ -39,7 +40,7 @@ for i in range(0, len(df)):
 			field = 1
 		elif j == 6:
 			field = 2
-		else field = "Invalid field"
+		else: field = "Invalid field"
 
 		# Find absolute path of where images are
 		home = '/home/ac833/Data/'
@@ -50,30 +51,46 @@ for i in range(0, len(df)):
 		# Change working directory to where the data is
 		os.chdir(cwd)
 
+		# Set up names of ap and alf files - have 5 alf and 5 ap files for each epoch
+		# Put them in two lists to make it easier to loop over later
+		ap_files = []
+		alf_files = []
+
+		for dither in range(j, j+5):
+			ap_files.append(stem + '_d' + str(dither) + '_cbcd_dn.ap')
+			alf_files.append(stem + '_d' + str(dither) + '_cbcd_dn.alf')
+
 		# Zero point
-		zero_point(i,j)
+		#zero_point(i,j)
 
 		# Standard aperture
-		std_aper(i,j)
+		#std_aper(i,j)
 
 		# Aperture correction
-		ap_corr(i,j)
+		#ap_corr(i,j)
 
 		# Location correction
-		loc_corr(i,j)
+		#loc_corr(i,j)
 
 # Zero point correction - applied to both ap and alf mags
-def zero_point():
+def zero_point(row_of_df, start_dither):
+
+	# Image name - only need to do it once as they are all using same telescope etc
+	image = stem + '_d1_cbcd_dn.fits'
+
+	# Load files
+
+
 	return(0)
 
 # Get onto standard aperture system - applied to ap mags only
 def std_aper():
 	return(0)
 
-# Aperture correction - uses ap and psf mags to calculate correction value then applies to psf mags only
+# Aperture correction - uses ap and alf mags to calculate correction value then applies to psf mags only
 def ap_corr():
 	return(0)
 
-# Location correction - only need to apply to psf mags as won't be using ap mags after this
+# Location correction - only need to apply to alf mags as won't be using ap mags after this
 def loc_corr():
 	return(0)
