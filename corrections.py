166e7f06144517170b54d61550e502e955c0edee
Abi Chown <A.H.Chown@bath.ac.uk>
2018-05-22 15:08:03 +0100

    Corrections actually correct! ;P

--- corrections.py
+++ corrections.py
@@ -1,5 +1,7 @@
 '''
 Purpose: Correct the instrumental magnitudes via zero point, standard aperture, aperture and location corrections
+Trying again as other script is a bit dodgy. Order of corrections in this script is location, then std aperture
+and zero mag, then aperture correction
 Written by: Abi Chown A.H.Chown@bath.ac.uk
 '''
 
@@ -9,6 +11,88 @@ from astropy.io import fits
 from math import log10 
 import os
 import sys
+
+# Location correction - apply to both individual alf files and ap files
+def loc_corr(row_of_df, start_dither):
+
+	# Open correction file corresponding to correct channel
+	if channel == 1:
+		corr_file = '/home/ac833/Spitzer-corr-images/ch1_al_s192.fits'
+		corr_image = fits.open(corr_file)
+		corr_data = corr_image[0].data
+	elif channel == 2:
+		corr_file = '/home/ac833/Spitzer-corr-images/ch2_al_s192.fits'
+		corr_image = fits.open(corr_file)
+		corr_data = corr_image[0].data
+	else: corr_file = 'Corr file not found'
+
+	# Loop over all 5 dithers and correct
+	for alf in alf_files:	
+
+		# File that needs correcting is the alf file as this is the first correction
+		# Load data into df
+		df = pd.read_csv(alf, header=None, skiprows=3, delim_whitespace=True, usecols=(0,1,2,3,4), names=['ID', 'X', 'Y', 'mag', 'error'])	
+
+		# Convert to flux, correct and convert back to mag for each star
+		for index in range(0, len(df)):
+
+			# Find the x and y coordinates corresponding to that star
+			x_coord = int(np.floor(float(df['X'][index])))
+			y_coord = int(np.floor(float(df['Y'][index])))
+
+			# Find the corr value at that (x_coord, y_coord) pixel
+			corr_val = corr_data[x_coord-1, y_coord-1]
+
+			# Convert to flux, apply corr val, and convert back to mag
+			if df['mag'][index] != 99.999:
+				flux = 10 ** (df['mag'][index]/-2.5) # convert to flux
+				flux = flux * corr_val # apply correction
+				df.loc[index, 'mag'] = -2.5 * log10(flux) # convert back to mag and update df
+
+		# Make new filename by replacing .alf with .alf_loc
+		alf = alf.replace('.alf', '.alf_loc')
+
+		# Write csv to file
+		df.to_csv(alf, sep=' ', index=False)
+
+	# Loop over all aperture files as these also need to be corrected
+	for ap in ap_files:	
+
+		# Load data - this is in df's because of the rubbish way .ap files are laid out
+		num_lines = sum(1 for line in open(ap)) # num lines in the ap file
+
+		first_rows = filter(lambda x: x%3 == 1, range(4, num_lines))
+		second_rows = filter(lambda x: x%3 == 2, range(4,num_lines))
+		third_rows = filter(lambda x: x%3 == 0, range(4, num_lines))
+
+		ap_firstlines = pd.read_csv(ap, skiprows=[0,1,2,3]+second_rows+third_rows, names=['ID', 'X', 'Y', 'mag'], delim_whitespace=True, header=None)
+		ap_secondlines = pd.read_csv(ap, skiprows=[0,1,2,3]+first_rows+third_rows, names=['Sky', 'Std sky', 'Skew sky', 'error'], delim_whitespace=True, header=None)
+
+		# Concat and drop unneccesary columns
+		ap_df = pd.concat((ap_firstlines, ap_secondlines), axis=1)
+		ap_df.drop(['Sky', 'Std sky', 'Skew sky'], inplace=True, axis=1)
+
+		# Convert to flux, correct and convert back to mag for each star
+		for index in range(0, len(ap_df)):
+
+			# Find the x and y coordinates corresponding to that star
+			x_coord = int(np.floor(float(ap_df['X'][index])))
+			y_coord = int(np.floor(float(ap_df['Y'][index])))
+
+			corr_val = corr_data[x_coord-1, y_coord-1]
+
+			if ap_df['mag'][index] != 99.999:
+				flux = 10 ** (ap_df['mag'][index]/-2.5) 
+				flux = flux * corr_val
+				ap_df.loc[index, 'mag'] = -2.5 * log10(flux)
+		
+		# Make new filename by replacing _apc with _all
+		ap = ap.replace('.ap', '.ap_loc')
+
+		# Write csv to file
+		ap_df.to_csv(ap, sep=' ', index=False)
+
+	return(0)
 
 # Zero point correction - applied to both ap and alf mags
 # Also standard aperture - applied to only ap mags
@@ -32,72 +116,61 @@ def zp_and_std_ape(row_of_df, start_dither):
 	zmag = round(2.5 * log10(F0/(fluxconv*px_ste)), 2)
 
 	# Load alf files, apply zmag and write out to file
-	for alf in field_alf_files:
+	# The files here are the .alf_loc files as location correction was just applied
+	for alf in alf_files:
+
+		# Filename we want is .alf_loc
+		alf = alf.replace('.alf', '.alf_loc')
 
 		# Load data
-		df = pd.read_csv(alf, header=None, names=['ID', 'X', 'Y', 'M1', 'E1', 'M2', 'E2', 'M3', 'E3', 'M4', 'E4', 'M5', 'E5', 'chi', 'sharp'], skiprows=3, delim_whitespace=True)
+		df = pd.read_csv(alf, header=0, delim_whitespace=True)
 
 		# Apply zmag to m (only to non 99 values)
 		for index in range(0, len(df)):
-			if df['M1'][index] != 99.9999:
-				df.loc[index, 'M1'] = df['M1'][index] - 25 + zmag
-			if df['M2'][index] != 99.9999:
-				df.loc[index, 'M2'] = df['M2'][index] - 25 + zmag				
-			if df['M3'][index] != 99.9999:
-				df.loc[index, 'M3'] = df['M3'][index] - 25 + zmag
-			if df['M4'][index] != 99.9999:
-				df.loc[index, 'M4'] = df['M4'][index] - 25 + zmag
-			if df['M5'][index] != 99.9999:
-				df.loc[index, 'M5'] = df['M5'][index] - 25 + zmag
+			if df['mag'][index] != 99.9999:
+				df.loc[index, 'mag'] = df['mag'][index] - 25 + zmag
 
 		# Write to new file
-		filename = alf.replace('.alf', '.alf_zp')
+		filename = alf.replace('.alf_loc', '.alf_zp') # to show std aper and zp has been applied
 		df.to_csv(filename, sep=' ', index=False)
 
 	# Load ap files, apply zmag and write out to file
 	for ap in ap_files:
 
+		# Filename we want is .ap_loc
+		ap = ap.replace('.ap', '.ap_loc')
+
 		# Load data - this is in df's because of the rubbish way .ap files are laid out
-		num_lines = sum(1 for line in open(ap)) # num lines in the ap file
-
-		first_rows = filter(lambda x: x%3 == 1, range(4, num_lines))
-		second_rows = filter(lambda x: x%3 == 2, range(4,num_lines))
-		third_rows = filter(lambda x: x%3 == 0, range(4, num_lines))
-
-		ap_firstlines = pd.read_csv(ap, skiprows=[0,1,2,3]+second_rows+third_rows, names=['ID', 'X', 'Y', 'Mag'], delim_whitespace=True, header=None)
-		ap_secondlines = pd.read_csv(ap, skiprows=[0,1,2,3]+first_rows+third_rows, names=['Sky', 'Std sky', 'Skew sky', 'Error'], delim_whitespace=True, header=None)
-
-		# Concat and drop unneccesary columns
-		ap_df = pd.concat((ap_firstlines, ap_secondlines), axis=1)
-		ap_df.drop(['Sky', 'Std sky', 'Skew sky'], inplace=True, axis=1)
+		ap_df = pd.read_csv(ap, header=0, delim_whitespace=True)
 
 		# Apply zmag to 'Mag' column
 		for z in range(0, len(ap_df)):
-			if ap_df['Mag'][z]!= 99.999:
-				ap_df.ix[z,'Mag'] = ap_df['Mag'][z] - 25 + zmag
+			if ap_df['mag'][z]!= 99.999:
+				ap_df.ix[z,'mag'] = ap_df['mag'][z] - 25 + zmag
 
 				# Convert mag to flux
-				flux = F0 * (10 ** (-ap_df['Mag'][z]/2.5))
+				flux = F0 * (10 ** (-ap_df['mag'][z]/2.5))
 
 				# Apply standard aperture calibration factor of 1.112
 				flux = 1.112 * flux
 
 				# Convert back to mag
-				ap_df.ix[z, 'Mag'] = -2.5 * log10(flux/F0)
+				ap_df.ix[z, 'mag'] = -2.5 * log10(flux/F0)
 
 		# Write to new file
-		filename = ap.replace('.ap', '.ap_zp')
+		filename = ap.replace('.ap_loc', '.ap_zp') # to show std aper and zp have been applied
 		f = open(filename, 'w')
-		f.write("ID  X  Y  Mag  Error \n")
+		f.write("ID  X  Y  mag  error \n")
 
 		for z in range(0, len(ap_df)):
-			f.writelines("%d %.3f %.3f %.4f %.4f \n" % (ap_df['ID'][z], ap_df['X'][z], ap_df['Y'][z], ap_df['Mag'][z], ap_df['Error'][z]))
+			f.writelines("%d %.3f %.3f %.4f %.4f \n" % (ap_df['ID'][z], ap_df['X'][z], ap_df['Y'][z], ap_df['mag'][z], ap_df['error'][z]))
 
 		f.close()
 
 	return(0)
 
 # Aperture correction - uses ap and als mags for dither 1 to calculate correction value then applies to alf mags only
+# Should it be the individual alf file for dither 1 instead of als file
 def ap_corr(row_of_df, start_dither):
 
 	# Get names for dither 1 als and lst files - don't need ap file because lst has ap mags
@@ -156,107 +229,23 @@ def ap_corr(row_of_df, start_dither):
 	# Calculate average difference
 	corr_val = round(df['Difference'].mean(), 3)
 
-	# Apply this correction value to all alf files
-	for alf in field_alf_files:
+	# Apply this correction value to all individual alf files
+	for alf in alf_files:
 
-		# Filename
-		filename = alf.replace('.alf', '.alf_zp')
+		# Filename is the alf_zp files as we have now corrected for loc and std aper and zp
+		alf = alf.replace('.alf', '.alf_zp')
 
 		# Open file into df
-		data = pd.read_csv(filename, delim_whitespace=True, header=0)
+		data = pd.read_csv(alf, delim_whitespace=True, header=0)
 
 		# Add corr_val to mag
 		for index in range(0, len(data)):
-			if data['M1'][index] != 99.9999:
-				data.loc[index, 'M1'] = data['M1'][index] + corr_val
-			if data['M2'][index] != 99.9999:
-				data.loc[index, 'M2'] = data['M2'][index] + corr_val				
-			if data['M3'][index] != 99.9999:
-				data.loc[index, 'M3'] = data['M3'][index] + corr_val
-			if data['M4'][index] != 99.9999:
-				data.loc[index, 'M4'] = data['M4'][index] + corr_val
-			if data['M5'][index] != 99.9999:
-				data.loc[index, 'M5'] = data['M5'][index] + corr_val
+			if data['mag'][index] != 99.9999:
+				data.loc[index, 'mag'] = data['mag'][index] + corr_val
 
-		# Output to a new file suffiz .alf_apc
-		filename = filename.replace('_zp', '_apc')
-		data.to_csv(filename, sep=' ', index=False)
-
-	return(0)
-
-# Location correction - only need to apply to alf mags as won't be using ap mags after this
-def loc_corr(row_of_df, start_dither):
-
-	# Open correction file corresponding to correct channel
-	if channel == 1:
-		corr_file = '/home/ac833/Spitzer-corr-images/ch1_al_s192.fits'
-		corr_image = fits.open(corr_file)
-		corr_data = corr_image[0].data
-	elif channel == 2:
-		corr_file = '/home/ac833/Spitzer-corr-images/ch2_al_s192.fits'
-		corr_image = fits.open(corr_file)
-		corr_data = corr_image[0].data
-	else: corr_file = 'Corr file not found'
-
-	# Loop over all 5 dithers and correct
-	for alf in field_alf_files:
-
-		# File that needs correcting is the alf_apc file
-		filename = alf.replace('.alf','.alf_apc')
-
-		# Load data into df
-		df = pd.read_csv(filename, header=0, delim_whitespace=True)
-
-		# Convert to flux, correct and convert back to mag for each star
-		for index in range(0, len(df)):
-
-			# Find the x and y coordinates corresponding to that star
-			x_coord = int(np.floor(float(df['X'][index])))
-			y_coord = int(np.floor(float(df['Y'][index])))
-
-			# Find the corr value at that (x_coord, y_coord) pixel
-			if (x_coord > 0 and x_coord < 256):
-				x_coord = x_coord
-			elif (x_coord <= 0):
-				x_coord = 0
-			else: x_coord = 255 # y_coord > 256
-
-			if (y_coord > 0 and y_coord < 256):
-				y_coord = y_coord
-			elif (y_coord <= 0):
-				y_coord = 0
-			else: y_coord = 255 # y_coord > 256
-
-			corr_val = corr_data[x_coord, y_coord]
-
-			# For each mag column, convert to flux, apply correction and convert back to flux
-			for index in range(0, len(df)):
-				if df['M1'][index] != 99.9999:
-					flux = 10 ** (df['M1'][index]/-2.5)
-					flux = flux * corr_val
-					df.loc[index, 'M1'] = -2.5 * log10(flux)
-				if df['M2'][index] != 99.9999:
-					flux = 10 ** (df['M2'][index]/-2.5)
-					flux = flux * corr_val
-					df.loc[index, 'M2'] = -2.5 * log10(flux)				
-				if df['M3'][index] != 99.9999:
-					flux = 10 ** (df['M3'][index]/-2.5)
-					flux = flux * corr_val
-					df.loc[index, 'M3'] = -2.5 * log10(flux)
-				if df['M4'][index] != 99.9999:
-					flux = 10 ** (df['M4'][index]/-2.5)
-					flux = flux * corr_val
-					df.loc[index, 'M4'] = -2.5 * log10(flux)
-				if df['M5'][index] != 99.9999:
-					flux = 10 ** (df['M5'][index]/-2.5)
-					flux = flux * corr_val
-					df.loc[index, 'M5'] = -2.5 * log10(flux)			
-
-		# Make new filename by replacing _apc with _all
-		filename = filename.replace('_apc', '_all')
-
-		# Write csv to file
-		df.to_csv(filename, sep=' ', index=False)
+		# Output to a new file suffix .alf_apc
+		alf = alf.replace('.alf_zp', '.alf_apc')
+		data.to_csv(alf, sep=' ', index=False)
 
 	return(0)
 
@@ -303,19 +292,17 @@ for i in range(0, len(df)):
 
 		print "Working on: " + str(target_name) + "    Epoch: " + str(epoch_number) + "     Field: " + str(field)
 
-		# Set up names of ap, alf and field alf files - have 5 alf and 5 ap files for each epoch and 2 field alf files
+		# Set up names of ap, alf and field alf files - have 5 alf and 5 ap files for each epoch and 1 field alf files
 		# Put them in three lists to make it easier to loop over later
 		ap_files = []
 		alf_files = []
-		field_alf_files = []
 
 		for dither in range(j, j+5):
 			ap_files.append(stem + '_d' + str(dither) + '_cbcd_dn.ap')
 			alf_files.append(stem + '_d' + str(dither) + '_cbcd_dn.alf')
 
-		for f in [1,2]:
-			field_alf_files.append(stem + '_f' + str(f) + '.alf')
-
+		# Location correction
+		loc_corr(i,j)
 
 		# Zero point
 		zp_and_std_ape(i,j)
@@ -323,5 +310,3 @@ for i in range(0, len(df)):
 		# Aperture correction
 		ap_corr(i,j)
 
-		# Location correction
-		loc_corr(i,j)
