d704fee58ad219826a79646eee7bf23ed9c94cfb
Abi Chown <A.H.Chown@bath.ac.uk>
2018-08-06 13:31:56 +0100

    Initial commit

---
+++ make_psf.py
@@ -0,0 +1,6 @@
+
+'''
+Purpose: Make the PSF model from the stars selected in the previous script (star_selection.py)
+Then run ALLSTAR to get PSF magnitudes for all the stars
+Written by: Abi Chown A.H.Chown@bath.ac.uk
+'''
